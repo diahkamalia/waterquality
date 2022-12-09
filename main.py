0ce9a1736087cd4bf1a1b21141845ea28b8ce81c
200411100061 - Diah Kamalia <101076428+diahkamalia@users.noreply.github.com>
2022-12-10 02:26:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -74,16 +74,16 @@ with desc:
 
     st.write("## Column Description")
     st.write("""
-    1. ph: pH of 1. water (0 to 14).
-    2. Hardness: Capacity of water to precipitate soap in mg/L.
-    3. Solids: Total dissolved solids in ppm.
-    4. Chloramines: Amount of Chloramines in ppm.
-    5. Sulfate: Amount of Sulfates dissolved in mg/L.
-    6. Conductivity: Electrical conductivity of water in μS/cm.
-    7. Organic_carbon: Amount of organic carbon in ppm.
-    8. Trihalomethanes: Amount of Trihalomethanes in μg/L.
-    9. Turbidity: Measure of light emiting property of water in NTU.
-    10. Potability: Indicates if water is safe for human consumption. Potable -1 and Not potable -0
+    1. ph: pH of 1. water (0 to 14) | **_Numerik_**.
+    2. Hardness: Capacity of water to precipitate soap in mg/L | **_Numerik_**.
+    3. Solids: Total dissolved solids in ppm | **_Numerik_**.
+    4. Chloramines: Amount of Chloramines in ppm | **_Numerik_**.
+    5. Sulfate: Amount of Sulfates dissolved in mg/L | **_Numerik_**.
+    6. Conductivity: Electrical conductivity of water in μS/cm | **_Numerik_**.
+    7. Organic_carbon: Amount of organic carbon in ppm | **_Numerik_**.
+    8. Trihalomethanes: Amount of Trihalomethanes in μg/L | **_Numerik_**.
+    9. Turbidity: Measure of light emiting property of water in NTU | **_Numerik_**.
+    10. Potability: Indicates if water is safe for human consumption. Potable -1 and Not potable -0 | **_Numerik_**.
     """)
     
     st.write("## Dataset Source")
